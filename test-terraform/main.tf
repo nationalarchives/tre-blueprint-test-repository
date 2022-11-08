dd564481c86c75484479cb16795035e494dd5676
rahmedzaizi <rahmed@zaizi.com>
2022-11-08 08:21:45 +0000

    test terraform backend

--- test-terraform/main.tf
+++ test-terraform/main.tf
@@ -9,8 +9,24 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+  }
 }
 
 resource "aws_s3_bucket" "foo" {
-  bucket = "test-github-actions"
+  bucket = "tre-test-workspace"
+}
+
+resource "aws_s3_bucket_public_access_block" "foo" {
+
+  bucket                  = aws_s3_bucket.foo.bucket
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "foo" {
+  bucket = aws_s3_bucket.foo.id
+  acl    = "private"
 }
