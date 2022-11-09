f3f793cfe8fbc8a91767a1c572bbe6756961c1c6
rahmedzaizi <rahmed@zaizi.com>
2022-11-09 12:41:22 +0000

    test-artifect

--- test-terraform/main.tf
+++ test-terraform/main.tf
@@ -12,21 +12,40 @@ terraform {
   backend "s3" {
   }
 }
-
-resource "aws_s3_bucket" "foo" {
-  bucket = "tre-test-workspace-2"
+resource "aws_s3_bucket" "common_tre_data" {
+  bucket = "${var.env}-tre-test-data"
 }
 
-resource "aws_s3_bucket_public_access_block" "foo" {
+resource "aws_s3_bucket_policy" "common_tre_data" {
+  bucket = aws_s3_bucket.common_tre_data.bucket
+  policy = data.aws_iam_policy_document.common_tre_data_bucket.json
+}
 
-  bucket                  = aws_s3_bucket.foo.bucket
+resource "aws_s3_bucket_acl" "common_tre_data" {
+  bucket = aws_s3_bucket.common_tre_data.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "common_tre_data" {
+  bucket = aws_s3_bucket.common_tre_data.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "common_tre_data" {
+  bucket = aws_s3_bucket.common_tre_data.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "common_tre_data" {
+  bucket                  = aws_s3_bucket.common_tre_data.bucket
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_acl" "foo" {
-  bucket = aws_s3_bucket.foo.id
-  acl    = "private"
 }
