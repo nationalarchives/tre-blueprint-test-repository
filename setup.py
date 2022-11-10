3555e3d28ff562addf579c598b92075d87d5c10d
Alex Ford <alex.ford@nationalarchives.gov.uk>
2022-11-10 13:35:13 +0000

    rename package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='testpackage',
+    name='tnatestpackage',
     version='0.1',
-    packages=['testpackage'],
+    packages=['tnatestpackage'],
     include_package_data=True,
     zip_safe=False
 )
