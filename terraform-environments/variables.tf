a789c8950dfeb8d0a60ba343c76e9ec7fb0ffc64
rahmedzaizi <rahmed@zaizi.com>
2022-11-14 12:59:24 +0000

    adding poc step function and lambda

--- terraform-environments/variables.tf
+++ terraform-environments/variables.tf
@@ -2,6 +2,10 @@ variable "env" {
   type = string
 }
 
+variable "prefix" {
+  type = string
+}
+
 variable "assume_role" {
   type = string
 }
---
+++ terraform-modules/tre-bp-poc/variables.tf
@@ -0,0 +1,11 @@
+variable "env" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "assume_role" {
+  type = string
+}
