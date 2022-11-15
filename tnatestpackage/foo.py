98ff59d375d3d2d8fc3f76814d1606f41d7885a9
Alex Ford <alex.ford@nationalarchives.gov.uk>
2022-11-15 10:29:15 +0000

    check the package updates easily

--- tnatestpackage/foo.py
+++ tnatestpackage/foo.py
@@ -1,2 +1,8 @@
 def event_python():
-    print("Did you really think you could destroy this Package? She's defied space and time. She's been to a place you couldn't possibly imagine. And now... it is time to go back.")
+    print("Did you really think you could destroy this package? She's defied space and time."
+          "She's been to a place you couldn't possibly imagine. And now... it is time to go back.")
+
+def event_python_2():
+    print("I created the Event Python to reach the CodeArtifact eepository, but she's gone much,"
+          " much farther than that. She tore a hole in our universe, a gateway to another dimension. "
+          "A dimension of pure chaos.")
