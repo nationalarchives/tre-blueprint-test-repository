4985556302e238ee770bbab6d23ed50cbbc08d4f
Alex Ford <alex.ford@nationalarchives.gov.uk>
2022-11-15 10:32:37 +0000

    version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tnatestpackage',
-    version='0.1',
+    version='0.2',
     packages=['tnatestpackage'],
     include_package_data=True,
     zip_safe=False
