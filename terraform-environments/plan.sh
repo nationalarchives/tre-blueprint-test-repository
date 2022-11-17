72c90afb143560272bc6a189143b6d2da9558c7c
rahmedzaizi <rahmed@zaizi.com>
2022-11-17 11:27:51 +0000

    test plan.sh script

--- terraform-environments/plan.sh
+++ terraform-environments/plan.sh
@@ -5,7 +5,7 @@ set -e
 echo
 function cleanup {
     status=$?; 
-    if [ $status != 0 ] && [ -f error.txt ]; then echo "Failure: $status" && python3 script/send_to_cw.py error.txt > /dev/null 2>&1; fi
+    if [ $status != 0 ] && [ -f error.txt ]; then echo "Failure: $status" && python3 script/send_to_cw.py error.txt; fi
 }
 
 trap cleanup EXIT
