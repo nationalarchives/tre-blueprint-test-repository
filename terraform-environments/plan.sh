1d689fa7c395674ad3d5f1153f3207399b7c7fd0
rahmedzaizi <rahmed@zaizi.com>
2022-11-17 10:58:41 +0000

    test plan.sh script

--- terraform-environments/plan.sh
+++ terraform-environments/plan.sh
@@ -10,11 +10,11 @@ function cleanup {
 trap cleanup EXIT
 
 terraform -v
-aws ssm get-parameters --name ${{ secrets.AWS_PARAM_STORE_TF_BACKEND_KEY }} --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
-aws ssm get-parameters --name ${{ secrets.AWS_PARAM_STORE_TF_VARS_KEY }} --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars 2> error.txt
+aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
+aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars 2> error.txt
 terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
 terraform workspace list
-terraform workspace select ${{ inputs.environment }}
+terraform workspace select "${ENV}"
 terraform plan -var-file="terraform.tfvars" -input=false -out plan.out > /dev/null 2> error.txt
 terraform show -no-color plan.out > plan.txt
-aws s3 cp plan.out s3://dev-te-testdata/tmp/${{ inputs.environment }}/${{ github.triggering_actor }}/plan.out
+aws s3 cp plan.out s3://dev-te-testdata/tmp/"${ENV}"/"${TRIGGERING_ACTOR}"/plan.out
