28a2f7e133c6de6ab253669e681fb9549c8b3435
rahmedzaizi <rahmed@zaizi.com>
2022-11-17 15:16:55 +0000

    test plan.sh script

--- terraform-environments/plan.sh
+++ terraform-environments/plan.sh
@@ -11,7 +11,6 @@ function cleanup {
 trap cleanup EXIT
 
 terraform -v
-pip install boto3
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars 2> error.txt
 terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
